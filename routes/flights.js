71b274a1fc5f402dc8ea3e7ed86c3bb103e43ee4
Gary Rivera <a.gary.rivera@gmail.com>
2021-07-30 10:00:53 -0700

    fleshed out a route for posting flight-offers

--- routes/flights.js
+++ routes/flights.js
@@ -17,7 +17,7 @@ const amadeus = new Amadeus({
 router.get(`/${API}/airports`, async (req, res) => {
   const { page, subType, keyword } = req.query;
   // API call with params we requested from client app
-
+  
   // Sending response for client
   try {
     const response = await amadeus.client.get("/v1/reference-data/locations", {
@@ -25,7 +25,8 @@ router.get(`/${API}/airports`, async (req, res) => {
       subType,
       "page[offset]": page * 10
     });
-    console.log(response.body)
+    console.log('INSIDE ROUTE', response.body)
+    // console.log(response.body)
     await res.json(JSON.parse(response.body));
   } catch (err) {
     await res.json(err);
@@ -34,24 +35,20 @@ router.get(`/${API}/airports`, async (req, res) => {
 
 
 // Flight Offers search
-// router.post(`/${API}/flight-offers`, async(req, res) => {
-//   try { response = await amadeus.client.post("/v1/shopping/flight-offers", {
-//     currencyCode: "USD",
-//     originDestinations: ,
-//     travelers: ,
-//     sources: ["GDS"],
-//     searchCriteria: {
-//       maxFlightOffers: 10,
-//       flightFilters: {
-//         cabin: ,
-//         originDestinationIds: [""]
-//       }
-//     }
-//   })
-//   }
-//   catch (err) {
-//     await res.json(err);
-//   }
-// }
+router.post(`/${API}/flight-offers`, async(req, res) => {
+  console.log('POST ROUTE')
+  departure = req.body.departure; 
+  arrival = req.body.arrival; 
+  locationDeparture = req.body.locationDeparture; 
+  locationArrival = req.body.locationArrival;
+  const response = await amadeus.shopping.flightOffersSearch 
+    .get({ 
+      originLocationCode: locationDeparture, 
+      destinationLocationCode: locationArrival, 
+      departureDate: departure, 
+      adults: "1", 
+    }) 
+    .catch((err) => console.log(err)); 
+});
 
 module.exports = router;
